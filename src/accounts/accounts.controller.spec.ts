6256bc53f774ab56f1a075266331ad6483d1ff06
samuraikun <tazitaziawawa@gmail.com>
2024-01-15 18:40:10 +0000

    Add PrismaService to providers to pass tests

--- src/accounts/accounts.controller.spec.ts
+++ src/accounts/accounts.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountsController } from './accounts.controller';
 import { AccountsService } from './accounts.service';
+import { PrismaService } from '../prisma.service';
 
 describe('AccountsController', () => {
   let controller: AccountsController;
@@ -8,7 +9,7 @@ describe('AccountsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AccountsController],
-      providers: [AccountsService],
+      providers: [AccountsService, PrismaService],
     }).compile();
 
     controller = module.get<AccountsController>(AccountsController);
--- src/accounts/accounts.service.spec.ts
+++ src/accounts/accounts.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountsService } from './accounts.service';
+import { PrismaService } from '../prisma.service';
 
 describe('AccountsService', () => {
   let service: AccountsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AccountsService],
+      providers: [AccountsService, PrismaService],
     }).compile();
 
     service = module.get<AccountsService>(AccountsService);
